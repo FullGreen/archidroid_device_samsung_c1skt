a2bae99cbf7787a64924300f6dffc697758d73a7
fullgreen <abin030187@naver.com>
2016-10-30 10:46:08 +0900

    c1skt: Modify RIL subclass for c1skt
    Signed-off-by: fullgreen <abin030187@naver.com>

--- c1skt.mk
+++ c1skt.mk
@@ -262,7 +262,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungExynos4RIL \
+    ro.telephony.ril_class=C1sktRIL \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
 
